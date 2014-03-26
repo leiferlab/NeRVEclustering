92bd74aaf0c4a31586437bb13acbad3228c1fa29
Jeffrey Nguyen <jnguyen@princeton.edu>
2014-03-26 13:03:10 -0400

    made a comment

--- WormSegmentHessian.m
+++ WormSegmentHessian.m
@@ -1,4 +1,5 @@
-
+%This is Jeff's amazing code!! We are so greatfule for jeff 
+% To work on it
 
 %% load George's file with manual identification
 A=importdata('/Users/jeffnguyen/Documents/Data/for andy/d1.tif.marker');
