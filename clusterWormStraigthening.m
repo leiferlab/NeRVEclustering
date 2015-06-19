ee83f0445c0bf6bb0e7d623aaea6e8ef4fbf8aed
Jeffrey Nguyen <jnguyen@princeton.edu>
2015-06-18 22:35:55 -0400

    added cluster version of straigtening, updated new versions of straightening that run a bit faster

--- clusterWormStraigthening.m
+++ clusterWormStraigthening.m
@@ -3,11 +3,12 @@ clusterFolder=['/scratch/tmp/jnguyen/' dataFolder];
 load([clusterFolder filesep 'startWorkspace.mat']);
 
 for iStack=nStart:(nStart+nRange-1)
-    
+   try 
 [~,pointStats,~,~]=...
     WormCLStraighten_5(clusterFolder,destination,vidInfo,...
     alignments,[],Vtemplate,vRegion,zOffset,iStack,side,0);
 
 save([dataFolder filesep  'pointStats' num2str(iStack,'%3.5d')],'pointStats');
-
+   catch me
+   end
 end
