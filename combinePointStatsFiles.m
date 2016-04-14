a5cfce317f2d7833cd1c777e0f3a0548fbf7be0c
Jeffrey Nguyen <jnguyen@princeton.edu>
2016-04-14 11:51:40 -0400

    small adjustments to combinePointStatsFiles to work on cluster

--- combinePointStatsFiles.m
+++ combinePointStatsFiles.m
@@ -1,16 +1,31 @@
 function pointStats=combinePointStatsFiles(psFolder,psLength)
 % creates a complete pointStats variable from an analysis folder which is
-% created by the WormStraightening programs.
+% created by the WormStraightening programs. Adjusted to work on the
+% cluster, DOES NOT OVERWRITE EXISTING POINTSTATS FILES
 if isempty(psFolder)
     psFolder=uipickfiles;
     psFolder=psFolder{1};
 end
-pointStats=repmat(struct(),1,psLength);
+dataFolder=fileparts(psFolder);
 pList=dir([psFolder filesep 'pointStats*']);
+if isempty(pList)
+    dataFolder=psFolder;
+    psFolder=dir([dataFolder filesep 'CLstratight*']);
+    pList=dir([psFolder filesep 'pointStats*']);
+end
 
-progressbar(0);
+if nargin==1
+    psLength=length(psList);
+end
+pointStats=repmat(struct(),1,psLength);
+
+%progressbar(0);
 for iFile=1:length(pList);
-    progressbar(iFile/length(pList));
+ %   progressbar(iFile/length(pList));
+ if ~mod(iFile,10)
+     display([ 'Completed frame ' num2str(iFile) ' of ' num2str(psLength)])
+ end
+ 
     idx=str2double(pList(iFile).name(11:15));
     input=load([psFolder filesep pList(iFile).name]);
     input=input.pointStats;
@@ -21,3 +36,4 @@ for iFile=1:length(pList);
     pointStats(idx).Rintensities=input.Rintensities;
     pointStats(idx).Volume=input.Volume;
 end
+save([dataFolder filesep 'PointsStats'],'pointStats');
