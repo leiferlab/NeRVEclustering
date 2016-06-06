566776bf40b0dd7b08c17dd44920ddf52adadd63
Jeffrey Nguyen <jnguyen@princeton.edu>
2016-06-06 00:59:17 -0400

    trying to debug trackcompiler when errors with straightening occur

--- clusterWormTrackCompiler.m
+++ clusterWormTrackCompiler.m
@@ -322,6 +322,9 @@ ccell=mat2cell(c2,nSelectAdd);
     input=subTranstionMatrix;
     input=bsxfun(@rdivide,input,sqrt(sum(input.^2,2)));
     input(isnan(input))=0;
+    size(input)
+    size(masterVec)
+
     %masterVec(masterVec<0)=5*masterVec(masterVec<0);
     
 aTest=masterVec'*input';
