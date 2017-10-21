4c0e604897bc24485ea428ad59017695e6d30f91
frandi <frandi@princeton.edu>
2017-10-21 11:01:54 -0400

    Increased minimum sphericity from 0.8 to 0.84

--- WormStraighteningCode/WormCLStraighten_11.m
+++ WormStraighteningCode/WormCLStraighten_11.m
@@ -30,7 +30,7 @@ options.thresh1=.05;
 options.minObjSize=50;
 options.maxObjSize=400;
 %minimum sphericity, long objects tend to get split in half. 
-options.minSphericity=.80; 
+options.minSphericity=.84; 
 options.filterSize=[10 10 4]; %3d bandpass filter, object size
 options.prefilter=1; %images are filtered before calling segmentation program
 options.hthresh=0;
